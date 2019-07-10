57b150bd6db0355a6bafb3f5f3399767cb2fa74e
Enoch Hang <enochhang@gmail.com>
2019-07-10 10:35:12 -0700

    Revised Code

--- soccer.py
+++ soccer.py
@@ -1,29 +1,69 @@
 # encoding=utf8
 
-# 2) The attached soccer.dat file contains the results from the English Premier League.
-#   The columns labeled ‘F’ and ‘A’ contain the total number of goals scored for 
-# and against each team in that season (so Arsenal scored 79 goals against opponents, 
-# and had 36 goals scored against them). Write a program to print the name of the 
-# team with the smallest difference in ‘for’ and ‘against’ goals.
+"""2) The attached soccer.dat file contains the results from 
+the English Premier League. The columns labeled ‘F’ and ‘A’ contain 
+the total number of goals scored for and against each team 
+in that season (so Arsenal scored 79 goals against opponents, 
+and had 36 goals scored against them). Write a program to print the 
+name of the team with the smallest difference in ‘for’ and ‘against’ goals.
+"""
 
-smallest = ["none", 1000]
-file = open("soccer.dat", 'r')
-for line in file:
-    row = line.split()
-    if (len(row) == 0):
-        continue
-    ateam = row[0].replace('.','') #to find which rows are teams
-    if ateam.isdigit():
-        against = row[6]
-        scored = row[8]
-        difference = abs(float(against)-float(scored))
-        if difference < smallest[1]: #compare with smallest difference
-            smallest = [row[1], difference]
-print("Team Name: " + str(smallest[0]))
-print("Smallest score difference: " + str(smallest[1]))
+_scored_for = 'F'
+_scored_against = 'A'
+_first_column = "Team"
+_team_infos = []
+_table_columns = []
+_smallest_diff = ["none", -1]
 
-# Is the way you wrote the second program influenced by writing the first?
 
-# Yes because the two prompts were similar. 
-# I figured that the best runtime would be O(n) and so I did the work while iterating.
-# The second program did simiarly
+def parse_data(filename):
+    file = open(filename, 'r')
+    for line in file:
+        line = line.replace('-', '')
+        row = line.split()
+        if not row:
+            continue
+        ateam = row[0].replace('.', '')
+        if ateam.isdigit():
+            row.pop(0)
+            create_team(row)
+        elif row[0] == _first_column:
+            populate_columns(row)
+    if _smallest_diff[1] != -1:
+        print ("Team: " + _smallest_diff[0])
+        print ("Smallest Diff: " + str(_smallest_diff[1]))
+
+
+def populate_columns(row):
+    for column in row:
+        _table_columns.append(column)
+
+
+def create_team(row):
+    global _smallest_diff
+    team_info = {}
+    for column in range(0, len(_table_columns)):
+        team_info[_table_columns[column]] = row[column]
+    diff = abs(float(team_info[_scored_against]) -
+               float(team_info[_scored_for]))
+    if diff < _smallest_diff[1]:
+        _smallest_diff = [team_info[_first_column], diff]
+    elif _smallest_diff[1] == -1:
+        _smallest_diff = [team_info[_first_column], diff]
+
+
+if __name__ == '__main__':
+    parse_data("soccer.dat")
+
+
+""" Is the way you wrote the second program influenced by writing the first?
+Yes because the two prompts were similar, but the datasets were different.
+The first had missing data. To handle this, I filled in the missing data with a 0
+to help with parsing the rest of the data in the dictionaries. The same with soccer. 
+Because of the way that I parsed the first program, I did similarly for the second 
+as I also used a similar technique.
+
+I figured that the best runtime would be O(n) and so I did the work while iterating.
+The second program did simiarly. But the first program takes longer than O(n) because
+I iterate through each line to clean data. 
+"""
--- weather.py
+++ weather.py
@@ -1,24 +1,93 @@
 # encoding=utf8
 
-# 1) In the attached file (w_data.dat), you’ll find daily weather data.   
-# Download this text file, then write a program to output the day number 
-# (column one) with the smallest temperature spread (the maximum temperature 
-# is the second column, the minimum the third column).
+""" 1) In the attached file (w_data.dat), you’ll find daily weather data.   
+Download this text file, then write a program to output the day number 
+(column one) with the smallest temperature spread (the maximum temperature 
+is the second column, the minimum the third column).
+"""
 
-normalday = [0,1000]
-file = open("w_data (5).dat", 'r')
-for line in file:
-    row = line.split()
-    if (len(row) == 0):
-        continue
-    if row[0].isdigit():
-        max = row[1].replace('*', '')
-        min = row[2].replace('*', '')
-        spread = abs(float(max)-float(min))
-        if spread < normalday[1]:
-            normalday = [row[0], spread]
-print("Day: " + str(normalday[0]))
-print("Smallest temperature spread: " + str(normalday[1]))
+_every_day_info = []
+_columns = []
+_column_width = []
+_first_column = "Dy"
+_max_column = "MxT"
+_min_column = "MnT"
+_lowest_spread = [0, -1]
 
 
-#Enoch Hang
+def populate_info(file_name):
+    file = open(file_name, 'r')
+    for line in file:
+        row = line.split()
+        if not row:
+            continue
+        if row[0].isdigit():
+            parse_row(line)
+        elif row[0] == _first_column:
+            populate_columns(line)
+    if _lowest_spread[1] != -1:
+        print("Day: " + str(_lowest_spread[0]))
+        print("Lowest Spread: " + str(_lowest_spread[1]))
+
+
+def populate_columns(line):
+    # While putting all column titles in _columns,
+    # find each column length to help with cleaning data
+    global _columns
+    global _column_width
+    _columns = line.split()
+    line = line.strip()
+    for curr in range(0, len(_columns)-1):
+        if curr-1 != len(_columns):
+            next_loc = line.find(_columns[curr+1])
+            spaces = next_loc
+            line = line[next_loc:]
+            _column_width.append(spaces)
+        else:
+            _column_width.append(len(_columns[curr]))
+
+
+def parse_row(line):
+    # Parses each line to clean data of empty columns
+    # by entering in a '0' in its place to match
+    # amount of columns
+    global _column_width
+    line = line.strip()
+    if int(line[0]+line[1]) < 10:
+        line = " " + line
+
+    curr_index = 0
+    curr_column = 0
+    for width in _column_width:
+        dataexists = False
+        for length in range(0, int(width)-1):
+            if line[curr_index+length] != ' ':
+                dataexists = True
+                break
+        if not dataexists:
+            line = line[0:curr_index] + '0' + line[curr_index+1:]
+        curr_index += int(width)
+    line = line.replace('*', '')
+    check_day(line.split())
+    # print line
+    # this line can be used to help with see what I did in this code
+
+
+def check_day(arr_of_data):
+    # Creates a dictionary for a day according to each column
+    # while checking if it is the lowest spread
+    global _lowest_spread
+    day_info = {}
+    for index in range(0, len(_columns)):
+        day_info[_columns[index]] = arr_of_data[index]
+    _every_day_info.append(day_info)
+    diff = abs(float(day_info[_max_column]) - float(day_info[_min_column]))
+    if diff < _lowest_spread[1]:
+        _lowest_spread = [day_info[_first_column], diff]
+    elif _lowest_spread[1] == -1:
+        _lowest_spread = [day_info[_first_column], diff]
+
+if __name__ == '__main__':
+    populate_info("w_data (5).dat")
+
+# Enoch Hang
